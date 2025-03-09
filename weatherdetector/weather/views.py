779f1ecccf32e0e4b0a584fedc1355574ca8d32a
Divyansh Sharma <136117452+Divyansh031@users.noreply.github.com>
2025-03-10 00:14:11 +0530

    Update views.py

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -8,7 +8,7 @@ def index(request):
     if request.method == 'POST':
         city = request.POST['city']
         encoded_city = urllib.parse.quote(city)
-        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+encoded_city+'&units=metric&appid=51c1fe13e7fac3ddc9650e9f2d0b8109').read() #after ? we pass parameters and q is query and = is used coz we are passing parameters
+        res = urllib.request.urlopen('YOUR_API').read() #after ? we pass parameters and q is query and = is used coz we are passing parameters
         json_data = json.loads(res)
         # Unit system: 'standard', 'metric', or 'imperial' Default to Kelvin i.e standard
         data  = {
